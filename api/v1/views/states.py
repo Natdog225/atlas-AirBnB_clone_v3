2596c7fb7810a88e2f4c7d182e63b9c556a05e68
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-11-10 11:15:59 -0600

    Updated states for better error handling.

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -47,17 +47,26 @@ def delete_state(state_id):
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def post_state():
     """ Creates a State """
-    if not request.get_json():
+    # Check if the Content-Type is application/json
+    if request.content_type != 'application/json':
+        abort(400, description="Content-Type must be application/json")
+
+    try:
+        data = request.get_json()
+    except Exception:
         abort(400, description="Not a JSON")
 
-    if 'name' not in request.get_json():
+    if not data:
+        abort(400, description="Empty JSON")
+
+    if 'name' not in data:
         abort(400, description="Missing name")
 
-    data = request.get_json()
     instance = State(**data)
     instance.save()
 
     return make_response(jsonify(instance.to_dict()), 201)
+
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
