7a8163d60aafd95148c6afac78f4ab6b1f85948c
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-11-10 18:14:16 -0600

    added documentation to users

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,67 +1,108 @@
 #!/usr/bin/python3
-from os import abort
-from urllib import request
+""" User view """
+from flask import jsonify, abort, request, make_response
 from api.v1.views import app_views
+from models import storage
 from models.user import User
-from flask import Flask, jsonify
 
-@app_views.route('/users', methods=['GET'])
+
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
 def get_users():
-    """
-    Retrieves the list of all User objects.
-    """
-    users = User.all()
-    return jsonify([user.to_dict() for user in users])
+    """Retrieves the list of all User objects.
 
-@app_views.route('/users/<user_id>', methods=['GET'])
+    Returns:
+        list: A list of dictionaries representing User objects.
+    """
+    all_users = storage.all(User).values()
+    list_users = []
+    for user in all_users:
+        list_users.append(user.to_dict())
+    return jsonify(list_users)
+
+
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def get_user(user_id):
+    """Retrieves a User object with the given ID.
+
+    Args:
+        user_id (str): The ID of the User object.
+
+    Returns:
+        dict: A dictionary representing the User object.
     """
-    Retrieves a User object by its ID.
-    """
-    user = User.get(user_id)
-    if user is None:
+    user = storage.get(User, user_id)
+    if not user:
         abort(404)
+
     return jsonify(user.to_dict())
 
-@app_views.route('/users', methods=['POST'])
-def create_user():
-    """
-    Creates a new User object.
-    """
-    data = request.get_json()
-    if not data:
-        abort(400, message="Missing data")
-    required_fields = ["email", "password"]
-    for field in required_fields:
-        if field not in data:
-            abort(400, message=f"Missing field: {field}")
-    user = User(email=data["email"], password=data["password"])
-    user.save()
-    return jsonify(user.to_dict()), 201
 
-@app_views.route('/users/<user_id>', methods=['PUT'])
-def update_user(user_id):
-    """
-    Updates a User object by its ID.
-    """
-    user = User.get(user_id)
-    if user is None:
-        abort(404)
-    data = request.get_json()
-    if data:
-        for key, value in data.items():
-            if key not in ["id", "email", "created_at", "updated_at"]:
-                setattr(user, key, value)
-        user.save()
-    return jsonify(user.to_dict())
-
-@app_views.route('/users/<user_id>', methods=['DELETE'])
+@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id):
+    """Deletes a User object with the given ID.
+
+    Args:
+        user_id (str): The ID of the User object.
+
+    Returns:
+        tuple: An empty dictionary and a 200 status code.
     """
-    Deletes a User object by its ID.
-    """
-    user = User.get(user_id)
-    if user is None:
+    user = storage.get(User, user_id)
+
+    if not user:
         abort(404)
-    user.delete()
-    return jsonify({})
+
+    storage.delete(user)
+    storage.save()
+
+    return make_response(jsonify({}), 200)
+
+
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def post_user():
+    """Creates a new User object.
+
+    Returns:
+        tuple: A dictionary representing the new User object and a 201 status code.
+    """
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+
+    if 'email' not in request.get_json():
+        abort(400, description="Missing email")
+
+    if 'password' not in request.get_json():
+        abort(400, description="Missing password")
+
+    data = request.get_json()
+    instance = User(**data)
+    instance.save()
+
+    return make_response(jsonify(instance.to_dict()), 201)
+
+
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+def put_user(user_id):
+    """Updates a User object with the given ID.
+
+    Args:
+        user_id (str): The ID of the User object.
+
+    Returns:
+        tuple: A dictionary representing the updated User object and a 200 status code.
+    """
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+
+    ignore = ['id', 'email', 'created_at', 'updated_at']
+
+    data = request.get_json()
+    for key, value in data.items():
+        if key not in ignore:
+            setattr(user, key, value)
+    storage.save()
+    return make_response(jsonify(user.to_dict()), 200)
