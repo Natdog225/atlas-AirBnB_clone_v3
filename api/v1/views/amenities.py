3649c22dfc779d7332dd2d02b38a6fc9d4669160
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-11-11 13:05:26 -0600

    imported json.

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -4,6 +4,7 @@ from flask import jsonify, abort, request, make_response
 from api.v1.views import app_views
 from models import storage
 from models.amenity import Amenity
+import json
 
 
 class AmenityEncoder(json.JSONEncoder):
