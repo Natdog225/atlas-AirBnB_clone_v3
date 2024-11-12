73363e2fbd7bc5aaf66dd1a7ab46d527a15b113a
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-11-12 15:37:18 -0600

    Whitespace fix.

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -34,7 +34,7 @@ class TestDBStorageDocs(unittest.TestCase):
         os.environ['HBNB_MYSQL_HOST'] = 'localhost'
         os.environ['HBNB_MYSQL_DB'] = 'hbnb_test_db'
         os.environ['HBNB_ENV'] = 'test'
-        
+
         cls.storage = DBStorage()
         cls.storage.reload()
         Base.metadata.create_all(cls.storage.__engine)
