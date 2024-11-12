6d9268c4e10e7e59dfa54c7c149097864c897f8b
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-11-12 10:57:32 -0600

    updated app.py for cors stuff. For some reason I cant download it on my machine. So testing it is gonna be interesting.

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,30 +1,36 @@
 #!/usr/bin/python3
-""" Flask Application """
-import sys
-import os
-from flask import Flask, make_response, jsonify
-from models import storage
+
+"""
+Main app module to start Flask for the API
+"""
+
+# No need to import HTTPException here
+from flask import Flask, jsonify
+from flask_cors import CORS
 from api.v1.views import app_views
+from models import storage
 
-
+# Initialize Flask
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def close_db(error):
-    """ Close Storage """
+def teardown_db(exception=None):
+    """Closes storage on teardown"""
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-    """ 404 Error """
-    return make_response(jsonify({'error': "Not found"}), 404)
+def not_found(error=None):
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
-    """ Main Function """
+    import os
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True, debug=True)
+    port = int(os.getenv('HBNB_API_PORT', '5000'))
+    app.run(host=host, port=port, threaded=True)
