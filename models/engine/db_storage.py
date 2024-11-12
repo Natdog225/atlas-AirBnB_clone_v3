f6495f291e06bf0041da83acd8277780167633db
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-11-12 15:23:47 -0600

    Trying to create a session in the beginning so needed tables are created

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -45,6 +45,10 @@ class DBStorage:
             Base.metadata.create_all(self.__engine)
         else:
             Base.metadata.create_all(self.__engine)
+        
+        sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sess_factory)
+        self.__session = Session()
 
     def all(self, cls=None):
         """query on the current database session"""
