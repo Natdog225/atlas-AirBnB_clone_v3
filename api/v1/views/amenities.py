f2808f9cbf0d1ee11c23f769ed0d8980b73c7331
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-11-13 12:52:55 -0600

    Made errors jsonify errors

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -43,26 +43,30 @@ def create_amenity():
     try:
         data = request.get_json()
     except Exception:
-        abort(400, description="Not a JSON")
+        return jsonify({"error": "Not a JSON"}), 400
+
     if 'name' not in data:
-        abort(400, description="Missing name")
+        return jsonify({"error": "Missing name"}), 400
+
     new_amenity = Amenity(**data)
     new_amenity.save()
     return jsonify(new_amenity.to_dict()), 201
-
 
 @app_views.route('/amenities/<amenity_id>', methods=['PUT'])
 def update_amenity(amenity_id):
     """Updates an Amenity by ID."""
     amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
-        abort(404)
+        return jsonify({"error": "Not found"}), 404
+
     try:
         data = request.get_json()
     except Exception:
-        abort(400, description="Not a JSON")
+        return jsonify({"error": "Not a JSON"}), 400
+
     for key, value in data.items():
         if key not in ['id', 'created_at', 'updated_at']:
             setattr(amenity, key, value)
+
     storage.save()
     return jsonify(amenity.to_dict()), 200
