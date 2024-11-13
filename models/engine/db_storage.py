5868dc85c57daae99c7e454b26a478df76265760
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-11-12 20:39:43 -0600

    ...

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -15,6 +15,10 @@ import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 Base = declarative_base()
 
@@ -29,57 +33,46 @@ class DBStorage:
 
     def __init__(self):
         """Instantiate a DBStorage object"""
+        logger.info("Initializing DBStorage...")
         HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
         HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
         HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
         HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
         HBNB_ENV = getenv('HBNB_ENV')
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
-                                      format(HBNB_MYSQL_USER,
-                                             HBNB_MYSQL_PWD,
-                                             HBNB_MYSQL_HOST,
-                                             HBNB_MYSQL_DB))
-        if HBNB_ENV == "test":
-            Base.metadata.drop_all(self.__engine)
+        
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+            HBNB_MYSQL_USER, HBNB_MYSQL_PWD, HBNB_MYSQL_HOST, HBNB_MYSQL_DB))
+        
+        logger.info("Creating tables...")
         Base.metadata.create_all(self.__engine)
+        
+        logger.info("Initializing session...")
         self.reload()
+        
+        logger.info("Verifying table existence...")
+        self.verify_tables_existence()
+
+    def verify_tables_existence(self):
+        """Check if tables exist and create them if they don't"""
+        inspector = sqlalchemy.inspect(self.__engine)
+        for table_name in classes.keys():
+            if not inspector.has_table(table_name.lower()):
+                logger.warning(f"Table {table_name} doesn't exist. Creating...")
+                Base.metadata.tables[table_name.lower()].create(self.__engine)
+                logger.info(f"Table {table_name} created.")
 
     def get(self, cls, id):
-        """Retrieves an object based on the class name and its ID.
-
-        Performs a query on the database to retrieve the matching row if it
-        exists and uses the columns to create the object for return.
-
-        Args:
-            cls (str): String representing the class name(Place, User, Amenity)
-            id: (str): UUID4 string representing the object ID.
-
-        Returns:
-            The object if it exists. None if cls or id is None or if the
-            object does not exist.
-        """
-        if cls is None or cls not in classes or id is None or type(id) is not \
-                str:
+        """Retrieves an object based on the class name and its ID."""
+        if cls is None or cls not in classes or id is None or type(id) is not str:
             return None
         cls = classes[cls]
         objs = self.__session.query(cls).filter(cls.id == id)
         if objs is None:
             return None
-        return (objs.first())
+        return objs.first()
 
     def count(self, cls=None):
-        """Retrieves the total number of object based on the class name.
-
-        Performs a query on the database to retrieve the matching row if it
-        exists and uses the columns to create the object for return.
-
-        Args:
-            cls (str): String representing the class name(Place, User, Amenity)
-
-        Returns:
-            The object if it exists. If cls is None, the total number of
-            objects stored is returned.
-        """
+        """Retrieves the total number of object based on the class name."""
         count = 0
         for clss in classes:
             if cls is None or cls is classes[clss] or cls is clss:
@@ -89,7 +82,7 @@ class DBStorage:
         return count
 
     def all(self, cls=None):
-        """query on the current database session"""
+        """Query on the current database session"""
         new_dict = {}
         for clss in classes:
             if cls is None or cls is classes[clss] or cls is clss:
@@ -97,28 +90,28 @@ class DBStorage:
                 for obj in objs:
                     key = obj.__class__.__name__ + '.' + obj.id
                     new_dict[key] = obj
-        return (new_dict)
+        return new_dict
 
     def new(self, obj):
-        """add the object to the current database session"""
+        """Add the object to the current database session"""
         self.__session.add(obj)
 
     def save(self):
-        """commit all changes of the current database session"""
+        """Commit all changes of the current database session"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete from the current database session obj if not None"""
+        """Delete from the current database session obj if not None"""
         if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
-        """reloads data from the database"""
+        """Reloads data from the database"""
         Base.metadata.create_all(self.__engine)
         sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sess_factory)
         self.__session = Session
 
     def close(self):
-        """call remove() method on the private session attribute"""
+        """Call remove() method on the private session attribute"""
         self.__session.remove()
