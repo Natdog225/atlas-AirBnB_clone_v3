588e3b377526ae587a562fa7fe1e476955643884
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-11-12 19:44:11 -0600

    Test

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -2,10 +2,8 @@
 """
 Contains the TestDBStorageDocs and TestDBStorage classes
 """
-
 from datetime import datetime
 import inspect
-from unittest.mock import Base
 import models
 from models.engine import db_storage
 from models.amenity import Amenity
@@ -17,6 +15,7 @@ from models.state import State
 from models.user import User
 import json
 import os
+import pep8
 import unittest
 DBStorage = db_storage.DBStorage
 classes = {"Amenity": Amenity, "City": City, "Place": Place,
@@ -27,19 +26,8 @@ class TestDBStorageDocs(unittest.TestCase):
     """Tests to check the documentation and style of DBStorage class"""
     @classmethod
     def setUpClass(cls):
-        """Set up for the test class"""
-        cls.storage = DBStorage()
-        cls.storage.reload()
-        Base.metadata.create_all(cls.storage.__engine)
-
-    def setUp(self):
-        """Set up for each test"""
-        self.storage = DBStorage()
-        self.storage.reload()
-
-    def tearDown(self):
-        """Tear down for each test"""
-        self.storage.__session.rollback()
+        """Set up for the doc tests"""
+        cls.dbs_f = inspect.getmembers(DBStorage, inspect.isfunction)
 
     def test_pep8_conformance_db_storage(self):
         """Test that models/engine/db_storage.py conforms to PEP8."""
@@ -84,16 +72,22 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_all_returns_dict(self):
         """Test that all returns a dictionaty"""
-        try:
-            self.assertIs(type(models.storage.all()), dict)
-        except Exception as e:
-            print(f"Exception occurred: {e}")
-            self.fail(f"Exception occurred: {e}")
-        try:
-            self.assertIs(type(models.storage.all()), dict)
-        except Exception as e:
-            print(f"Exception occurred: {e}")
-            self.fail(f"Exception occurred: {e}")
+        self.assertIs(type(models.storage.all()), dict)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_returns_obj(self):
+        """Test that get returns an existing object """
+        state = State(name="California")
+        state.save()
+        first_state_obj = list(models.storage.all("State").values())[0]
+        state_obj = models.storage.get("State", first_state_obj.id)
+        self.assertIs(first_state_obj, state_obj)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_returns_none(self):
+        """Test that get returns None for nonexisting object """
+        state_obj = models.storage.get("State", "IDONTEXIST")
+        self.assertIsNone(state_obj)
 
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_all_no_class(self):
@@ -108,34 +102,11 @@ class TestFileStorage(unittest.TestCase):
         """Test that save properly saves objects to file.json"""
 
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-    def test_get(self):
-        """Test that get properly retrieves one object, on class and ID"""
-        storage = DBStorage()
-        storage.reload()
-        for key, value in classes.items():
-            obj = value()
-            storage.new(obj)
-            storage.save()
-            test_obj = storage.get(value, obj.id)
-            self.assertIsNotNone(test_obj)
-            self.assertEqual(obj.id, test_obj.id)
-            self.assertEqual(obj.__class__, test_obj.__class__)
-            self.assertEqual(obj.to_dict(), test_obj.to_dict())
-
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_count(self):
-        """Test that count properly counts the number of objects in storage"""
-        storage = DBStorage()
-        storage.reload()
-        count = 0
-        for value in classes.values():
-            obj = value()
-            storage.new(obj)
-            storage.save()
-            count += 1
-        self.assertEqual(count, storage.count())
-        self.assertEqual(storage.count(State), 1)
-        self.assertEqual(storage.count(City), 1)
-        self.assertEqual(storage.count(Amenity), 1)
-        self.assertEqual(storage.count(Place), 1)
-        self.assertEqual(storage.count(Review), 1)
+        """Test that count is properly return """
+        objs = models.storage.all()
+        self.assertEqual(len(objs), models.storage.count())
+        state_objs = models.storage.all("State")
+        self.assertEqual(len(state_objs), models.storage.count("State"))
+        no_objs = models.storage.all("Any")
+        self.assertEqual(len(no_objs), models.storage.count("Any"))
