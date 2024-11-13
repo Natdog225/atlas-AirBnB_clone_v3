9e5211bd660be4518d9b6c8d8a70129ac612941c
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-11-13 14:45:17 -0600

    pep

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -42,10 +42,10 @@ class TestFileStorageDocs(unittest.TestCase):
     def test_pep8_conformance_test_file_storage(self):
         """Test test_file_storage.py conforms to PEP8."""
         pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files
-        (['tests/test_models/test_engine/test_file_storage.py'])
-        self.assertEqual(result.get_count(), 0,
-                         "Found code style errors (and warnings).")
+        result = pep8s.check_files(['tests/test_models/test_engine/test_file_storage.py'])
+        self.assertEqual(result.total_errors, 0,
+                        "Found code style errors (and warnings).")
+
 
     def test_file_storage_module_docstring(self):
         """Test for the file_storage.py module docstring"""
