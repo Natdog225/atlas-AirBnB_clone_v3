d3fa3ed995701801bb9976d888ad322b8ed3b5a9
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-11-12 18:05:14 -0600

    back to two hours ago db

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -46,10 +46,6 @@ class DBStorage:
         else:
             Base.metadata.create_all(self.__engine)
 
-        sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(sess_factory)
-        self.__session = Session()
-
     def all(self, cls=None):
         """query on the current database session"""
         new_dict = {}
